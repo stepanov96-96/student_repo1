8a2ee23770ee29c2e04701b06fcbad3c683bbd01
Owerch <sasanikonenko53874@gmail.com>
2024-02-17 20:30:57 +0700

    f

--- lesson 1/ 3.py
+++
@@ -1,5 +0,0 @@
-import math
-
-r = int(input('Ведите радиус круга, что бы узнать его площадь: '))
-s = math.pi * math.pow(r, 2)
-print('Площадь круга с радиусом ' + str(r) + ' равна', round(s))
--- lesson 1/ 4.py
+++
@@ -1,4 +0,0 @@
-num1 = input('Введите число №1: ')
-num2 = input('Введите число №2: ')
-print(int(num1) + int(num2))
-print(float(num1) + float(num2))
