31701e5099955f86b16e8753d8a9c3710a1bdbcc
ITech777 <157210341+ITech777@users.noreply.github.com>
2024-02-24 18:09:35 +0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,6 @@
+import math
+radius = float(input("Введите радиус круга: "))
+# Вычисляем площадь круга
+area = math.pi * (radius ** 2)
+# Выводим результат
+print("Площадь круга с радиусом", radius, "равна", area)
