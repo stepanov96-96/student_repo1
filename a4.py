fd5da19438c2593955260fd55b7e66e7af441f2e
sandiegoforword <157867547+sandiegoforword@users.noreply.github.com>
2024-02-24 18:16:59 +0700

    Add files via upload

---
+++ a4.py
@@ -0,0 +1,9 @@
+num1_str = input("Введите первое число: ")
+num2_str = input("Введите второе число: ")
+
+num1 = float(num1_str)
+num2 = float(num2_str)
+
+sum_result = num1 + num2
+
+print(f"Сумма введенных чисел {num1} и {num2} равна {sum_result}")
