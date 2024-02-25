e7304063a86e7b4783c5fc65f52834f379c25412
Dasssha09 <157128967+Dassq@users.noreply.github.com>
2024-02-25 21:59:17 +0700

    Update lessson1-1.py

--- lessson1-1.py
+++ lessson1-1.py
@@ -1,9 +1,6 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> name = str(input(' Enter your name'))
- Enter your name Dasha
->>> age = int(input(' Enter your age '))
- Enter your age 18
->>> print(f' Hello, (name) you are (age) years old')
- Hello, (name) you are (age) years old
->>>
+name = str(input(' Enter your name'))
+
+age = int(input(' Enter your age '))
+
+print(f' Hello, (name) you are (age) years old')
+
