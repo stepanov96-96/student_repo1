ee0c3bba590af5106aa04deb0eb0f0be3a340b40
Dasssha09 <157128967+Dassq@users.noreply.github.com>
2024-02-25 21:55:30 +0700

    Update lesson1-2.py

--- lesson1-2.py
+++ lesson1-2.py
@@ -1,13 +1,3 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> a = int(input(' Enter first number '))
- Enter first number 12
->>> b = int(input(' Enter the second number: '))
- Enter the second number: 5
->>> print(f' sum - {a+b}\n diff - {a-b}\n multiplier - {a*b}\n split - {a/b}\n remaider of devision - {a%b}')
- sum - 17
- diff - 7
- multiplier - 60
- split - 2.4
- remaider of devision - 2
->>>
+a = int(input(' Enter first number '))
+b = int(input(' Enter the second number: '))
+print(f' sum - {a+b}\n diff - {a-b}\n multiplier - {a*b}\n split - {a/b}\n remaider of devision - {a%b}')
