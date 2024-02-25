4fd6349d547e98af4608f17c24674f35bdb79630
Dasssha09 <157128967+Dassq@users.noreply.github.com>
2024-02-25 21:56:56 +0700

    Update lesson1-4.py

--- lesson1-4.py
+++ lesson1-4.py
@@ -1,11 +1,6 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license" for more information.
->>> import math
->>> a = str(input(' Enter first number '))
- Enter first number 5
->>> b = str(input(' Enter the second number: '))
- Enter the second number: 7
->>> a = int(a); b = int(b)
->>> print(f' sum - {a+b}')
- sum - 12
->>>
+import math
+a = str(input(' Enter first number '))
+b = str(input(' Enter the second number: '))
+a = int(a); b = int(b)
+print(f' sum - {a+b}')
+
